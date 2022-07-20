fa8f9011254729112406109d178424592abb6076
Eddie-Kuo <Eddiekuo97@gmail.com>
2022-07-20 18:35:04 -0500

    No new features added without having to copy and paste code. Will revisit in the future

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const nameInput = document.getElementById('name-input')
 const button = document.getElementById('btn')
 const nameDiv = document.getElementById('name')
 
+const headerColor = document.getElementById('header')
+const footerColor = document.getElementById('footer')
+
 // let state
 
 // set event listeners 
@@ -11,8 +14,8 @@ const nameDiv = document.getElementById('name')
 button.addEventListener('click', () => {
   const result = nameInput.value;
   nameDiv.textContent = result
-}); 
 
+}); 
 
   // get user input
   // use user input to update state 

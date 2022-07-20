e1c8270ac1a9f30d3f693c680c1f365e653862d4
Eddie-Kuo <Eddiekuo97@gmail.com>
2022-07-20 17:45:16 -0500

    Basic CSS styling and basic JS interaction

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 // import functions and grab DOM elements
 
+const nameInput = document.getElementById('name-input')
+const button = document.getElementById('btn')
+const nameDiv = document.getElementById('name')
+
 // let state
 
 // set event listeners 
+
+button.addEventListener('click', () => {
+  const result = nameInput.value;
+  nameDiv.textContent = result
+}); 
+
+
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
